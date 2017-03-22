ef50172cbd9598a55d28fbf126b16fe27b557fa9
Luke Schuyler <lukeschuyler1@gmail.com>
2017-03-22 16:04:56 -0500

    added second way with createReadstream

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const http = require('http')
 const server = http.createServer()
 const { readFile, createReadStream } = require('fs')
-const readStream = createReadStream('index.html')
 
 server.on('request', ({ url, method, headers }, res) => {
     console.log('Request received for:', url)
@@ -9,16 +8,22 @@ server.on('request', ({ url, method, headers }, res) => {
     const path = url.slice(-1) === '/' 
     ? url.slice(1).concat('index.html') 
     : url.slice(1)
-    console.log('request received for: ', path);
 
-    readFile(path, (err, buff) => {
-     if(err){
-      res.statusCode = 404;
-      console.log('res', res)
-      res.end('not found nodey\n')
-     }
-    res.end(buff)
-  })
+
+    const readStream = createReadStream(path)
+
+      readStream.on('open', function () {
+	    readStream.pipe(res);
+	  });
+
+  //   readFile(readStream, (err, buff) => {
+  //    if(err){
+  //     res.statusCode = 404;
+  //     console.log('res', res)
+  //     res.end('not found nodey\n')
+  //    }
+  //   res.end(buff)
+  // })
 })
 
 server.listen(8080)
